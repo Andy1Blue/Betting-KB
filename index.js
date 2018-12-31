e6a239b3a1c7270149bdea8d721c7b8322dc5e5c
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2018-12-31 17:40:04 +0100

    include /admin route handler in different file

--- index.js
+++ index.js
@@ -12,39 +12,10 @@ const helpers = require('./lib/helpers');
 const _data = require('./lib/dataFs');
 const config = require('./config');
 const app = express();
-const admin = require('./lib/admin');
 const bet = require('./lib/bet')
 
-// Init admin
-//Required data: token, email?, // userAccess (1 - admin, 0 - user)
-app.get('/admin', (req, res) => {
-  //Get the token from the headers
-  const token = typeof (req.headers.token) == 'string' && req.headers.token.length == 20 ? req.headers.token : false;
-  var userAccess;
-  db.read('user', 'email', token.email)
-    .then(userData => {
-      userAccess = userData[0].access;
-    });
-  if (token && userAccess === 1) {
-    helpers.verifyToken(token, email, (tokenIsValid) => {
-
-      if (tokenIsValid) {
-        res.writeHead(200, {
-          'Content-Type': 'application/json'
-        });
-        res.write(JSON.stringify({
-          'OK': 'Yes!'
-        }));
-        res.end();
-      }
-    });
-  } else {
-    helpers.response(res, 403, {
-      'Error': 'Token is missing or access is not admin'
-    });
-  }
-});
-/////////////////////////
+//Get /admin
+require('./lib/admin')(app);
 
 let session = [];
 
