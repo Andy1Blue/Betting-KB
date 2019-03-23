af1e99a5633054568b6337ff9b15604935911571
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-03-23 18:08:28 +0100

    Adding main page, re-arrangement of files

---
+++ frontend/client/src/components/App/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from '../../assets/logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <h1>
+              Betting
+            </h1>
+            <h2>
+            Bet your match
+            </h2>
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
