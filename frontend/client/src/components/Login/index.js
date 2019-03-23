e0a7422b4bc800c69eaee5cfe49e3e7fd7dbe7eb
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-03-23 18:23:02 +0100

    Adding style to Login component

--- frontend/client/src/components/Login/index.js
+++ frontend/client/src/components/Login/index.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class Main extends Component {
   render() {
     return (
     <p className="App-login">
-      <form method="POST" onSubmit={this.loginUser}>
-        <p>Email:</p>
-        <input type="text" name="email"/><br/>
-        <p>Password:</p>
-        <input type="password" name="password"/><br/>
-        <button>Login</button>
-      </form>
-      </p>
+      <div className="Login-form">
+        <form method="POST" onSubmit={this.loginUser}>
+          <p>Email:</p>
+          <input type="text" name="email"/><br/>
+          <p>Password:</p>
+          <input type="password" name="password"/><br/>
+          <button>Login</button>
+        </form>
+      </div>
+    </p>
   )
 }
 
