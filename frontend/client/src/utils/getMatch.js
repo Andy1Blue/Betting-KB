393b7d19e3dfbe8ad01f6fca6008fa93cdf829cf
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-04-06 21:24:18 +0200

    Adding Matches component and getMatch util

---
+++ frontend/client/src/utils/getMatch.js
@@ -0,0 +1,20 @@
+export default function getMatch(token, email) {
+    return new Promise((resolve, reject) => {
+      fetch('http://localhost:3322/match',{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'token': token,
+          'email': email
+        }
+      })
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error)
+      });
+    });
+  }
